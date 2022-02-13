f31897bd86f031216c5791f6b248725dcc8e3fc2
mariahharilall <mariahharilall@gmail.com>
2022-02-13 17:01:44 -0500

    modify extension to dictionary

---
+++ popup/searchionary.js
@@ -0,0 +1,15 @@
+function myFunction(){
+    const word = document.getElementById("word").value;
+    searchWord(word);
+}
+
+function searchWord(word){
+    // check for valid word
+    let alphaCheck = /^[a-zA-Z]+$/.test(word);
+    // check that word length is greater than 0 and only contains letters
+    if (alphaCheck == true && word.length > 0){
+        const URL = `https://www.merriam-webster.com/dictionary/${word}`;
+        window.open(URL, "_blank");
+    }
+
+}

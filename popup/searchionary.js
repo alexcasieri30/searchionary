de4437d15d4ee3aa3318dfbc6f940a05bbda3c13
mariahharilall <mariahharilall@gmail.com>
2022-02-13 19:08:31 -0500

    fix extensions ability to open URL

--- popup/searchionary.js
+++ popup/searchionary.js
@@ -1,15 +1,16 @@
-function myFunction(){
+document.getElementById("submit").addEventListener("click", function(){ 
     const word = document.getElementById("word").value;
     searchWord(word);
-}
+});
 
 function searchWord(word){
     // check for valid word
+    console.log(word);
     let alphaCheck = /^[a-zA-Z]+$/.test(word);
     // check that word length is greater than 0 and only contains letters
     if (alphaCheck == true && word.length > 0){
         const URL = `https://www.merriam-webster.com/dictionary/${word}`;
-        window.open(URL, "_blank");
+        window.open(
+            URL, "_blank");
     }
-
 }

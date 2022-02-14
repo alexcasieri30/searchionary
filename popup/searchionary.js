b1eca41a5d1ad02838dca41f9f1b5cddf4198385
mariahharilall <mariahharilall@gmail.com>
2022-02-13 19:55:19 -0500

    add additional comments

--- popup/searchionary.js
+++ popup/searchionary.js
@@ -1,16 +1,17 @@
+// add click event listener for when the submit button is clicked
 document.getElementById("submit").addEventListener("click", function(){ 
     const word = document.getElementById("word").value;
     searchWord(word);
 });
 
+// given the user input, search the word
 function searchWord(word){
     // check for valid word
-    console.log(word);
     let alphaCheck = /^[a-zA-Z]+$/.test(word);
     // check that word length is greater than 0 and only contains letters
     if (alphaCheck == true && word.length > 0){
+        // navigate to URL of merriam webster page for the given word inputted by the user
         const URL = `https://www.merriam-webster.com/dictionary/${word}`;
-        window.open(
-            URL, "_blank");
+        window.open(URL, "_blank");
     }
 }

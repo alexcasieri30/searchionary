26dce50295054c3ed3f61b6e65c7b94b70339636
alexcasieri <ajc880@nyu.edu>
2022-05-02 08:56:57 -0400

    added contribution

--- popup/searchionary.js
+++ popup/searchionary.js
@@ -1,17 +1,35 @@
 // add click event listener for when the submit button is clicked
 document.getElementById("search").addEventListener("click", function(){ 
     const word = document.getElementById("word").value;
-    searchWord(word);
+    var t = searchWord(word);
+    const definition = document.getElementById("definition");
+    var els = t.querySelectorAll("span.dtText");
+    for (let i = 0; i < els.length; i++){
+        let text = els[i].innerHTML;
+        definition.innerHTML += text;
+    }
 });
 
 // given the user input, search the word
 function searchWord(word){
+    const definition = document.getElementById("definition");
+    definition.innerHTML = "Definition";
     // check for valid word
     let alphaCheck = /^[a-zA-Z]+$/.test(word);
     // check that word length is greater than 0 and only contains letters
     if (alphaCheck == true && word.length > 0){
         // navigate to URL of merriam webster page for the given word inputted by the user
         const URL = `https://www.merriam-webster.com/dictionary/${word}`;
-        window.open(URL, "wFormx", "width=800,height=600,location=no,menubar=no,status=no,titilebar=no,resizable=no,");
+        var text = getSourceAsDOM(URL);
+        return text;
     }
+}
+
+function getSourceAsDOM(url)
+{
+    xmlhttp=new XMLHttpRequest();
+    xmlhttp.open("GET",url,false);
+    xmlhttp.send();
+    parser=new DOMParser();
+    return parser.parseFromString(xmlhttp.responseText,"text/html");      
 }
